8f460c0cf6a27e48c6012ea025b5dd08813a3e2d
Rene Jimenez <rji@synapsis-group.com>
2022-06-09 15:30:05 +0200

    Adding Node and Edge classes

---
+++ Edge.cs
@@ -0,0 +1,8 @@
+namespace SytyRouting
+{
+    public class Edge
+    {
+        public long Id {get; set;}
+        public Node? EndNode {get; set;}
+    }
+}
---
+++ Node.cs
@@ -0,0 +1,10 @@
+namespace SytyRouting
+{
+    public class Node
+    {
+        public long Id {get; set;}
+        public double X {get; set;}
+        public double Y {get; set;}
+        public List<Edge>? TargetEdges {get; set;}
+    }
+}
