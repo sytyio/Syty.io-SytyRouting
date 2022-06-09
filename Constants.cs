3553f979a304930983aabd7846b1e93b259f3f80
Rene Jimenez <rji@synapsis-group.com>
2022-06-09 15:15:02 +0200

    Adding a Constants class

---
+++ Constants.cs
@@ -0,0 +1,7 @@
+namespace SytyRouting
+{
+    public static class Constants
+    {
+        public const string connectionString = "Host=compute.syty.io:1234;Username=postgres;Password=test123;Database=sytyrun";
+    }
+}
