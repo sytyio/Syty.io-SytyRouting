97c5d89f6bd48fcd12b28961e349c7113aafec4a
Rene Jimenez <rji@synapsis-group.com>
2022-06-13 14:39:30 +0200

    Adding 'one_way' states enum

--- Constants.cs
+++ Constants.cs
@@ -3,5 +3,13 @@ namespace SytyRouting
     public static class Constants
     {
         public const string connectionString = "Host=compute.syty.io:1234;Username=postgres;Password=test123;Database=sytyrun";
+
+        public enum OneWayState
+        {
+            Reversed = -1,
+            Unknown = 0,
+            Yes = 1,
+            No = 2,
+        }
     }
 }
