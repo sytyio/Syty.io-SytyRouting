676d0b57275fd209b3e7654e8ea5b9fa8a8480ec
Rene Jimenez <rji@synapsis-group.com>
2022-06-14 14:14:38 +0200

    Enum OneWayState simplified

--- Constants.cs
+++ Constants.cs
@@ -1,15 +1,16 @@
 namespace SytyRouting
 {
+
+    public enum OneWayState
+    {
+        Reversed = -1,
+        Unknown = 0,
+        Yes = 1,
+        No = 2,
+    }
+
     public static class Constants
     {
-        public const string connectionString = "Host=compute.syty.io:1234;Username=postgres;Password=test123;Database=sytyrun";
-
-        public enum OneWayState
-        {
-            Reversed = -1,
-            Unknown = 0,
-            Yes = 1,
-            No = 2,
-        }
+        public const string connectionString = "Host=compute.syty.io:1234;Username=postgres;Password=test123;Database=sytyrun";        
     }
 }
