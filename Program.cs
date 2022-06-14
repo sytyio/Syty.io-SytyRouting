7692f1bed5d9e62a37af7dcb3558b49acf862425
Rene Jimenez <rji@synapsis-group.com>
2022-06-14 11:08:42 +0200

    Adding a Log manager

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
-﻿
+﻿using NLog;
+
 namespace SytyRouting
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         static async Task Main(string[] args)
         {
-            Console.WriteLine("syty.io routing engine for large scale datasets");
+            // Logger configuration
+            NLog.Common.InternalLogger.LogLevel = NLog.LogLevel.Debug;
+            NLog.Common.InternalLogger.LogToConsole = true;
+
+            logger.Info("syty.io routing engine for large scale datasets");
+
+            // Console.WriteLine("syty.io routing engine for large scale datasets");
+
             var graph = new Graph();
             await graph.DBLoadAsync();
             graph.GetNodes();
+
+            // Logger flushing
+            LogManager.Shutdown();
         }
     }
 }
