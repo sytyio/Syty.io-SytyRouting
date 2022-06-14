31af36bfa35defed2cbfb10d2d7b7f1b35316a3a
Rene Jimenez <rji@synapsis-group.com>
2022-06-14 14:58:48 +0200

    Adding a stop constant for logging information

--- Constants.cs
+++ Constants.cs
@@ -11,6 +11,8 @@ namespace SytyRouting
 
     public static class Constants
     {
-        public const string connectionString = "Host=compute.syty.io:1234;Username=postgres;Password=test123;Database=sytyrun";        
+        public const string connectionString = "Host=compute.syty.io:1234;Username=postgres;Password=test123;Database=sytyrun";
+
+        public const ulong logStopIterations = 10;
     }
 }
