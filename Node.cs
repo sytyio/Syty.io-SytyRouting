e16f86ed15fe3cecd32eb3ae16286f25b16a5c41
Rene Jimenez <rji@synapsis-group.com>
2022-06-16 09:18:36 +0200

    Changing Edge List names and adding Inward Edges list

--- Node.cs
+++ Node.cs
@@ -5,6 +5,7 @@ namespace SytyRouting
         public long Id {get; set;}
         public double X {get; set;}
         public double Y {get; set;}
-        public List<Edge> TargetEdges {get; set;} = new List<Edge>();
+        public List<Edge> InwardEdges {get; set;} = new List<Edge>();
+        public List<Edge> OutwardEdges {get; set;} = new List<Edge>();
     }
 }
