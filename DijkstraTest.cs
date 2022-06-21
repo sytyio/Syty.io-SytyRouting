6b6b548e99bf8ab1a9bb53c01932a567cf2bc42d
Rene Jimenez <rji@synapsis-group.com>
2022-06-21 17:08:34 +0200

    Preliminary GetRoute method

--- DijkstraTest.cs
+++ DijkstraTest.cs
@@ -50,6 +50,80 @@ namespace SytyRouting
             }
         }
 
+        public List<Node> GetRoute(long originNodeOsmId, long destinationNodeOsmId)
+        {
+            var route = new List<Node>();
+            int i = 0;
+            int j = 0;
+
+            Dictionary<int, Node> visitedNodes = new Dictionary<int, Node>();
+            Dictionary<int, DijkstraStep> dijkstraSteps = new Dictionary<int, DijkstraStep>();
+            // Dictionary<int, DijkstraStep> dijkstraStepsOrdered = new Dictionary<int, DijkstraStep>();
+
+            var originNode = Array.Find(nodes, n => n.OsmID == originNodeOsmId);
+            var destinationNode = Array.Find(nodes, n => n.OsmID == destinationNodeOsmId);
+            if(originNode == null)
+            {
+                logger.Info("Couldn't find origin node (source_osm = {0})", originNodeOsmId);
+            }
+            else if(destinationNode == null)
+            {
+                logger.Info("Couldn't find destination node (source_osm = {0})", destinationNodeOsmId);
+            }
+            else
+            {
+                logger.Info("Origin Node: OsmId={0}, Idx={1}", originNode?.OsmID, originNode?.Idx);
+
+                var firstStep = new DijkstraStep{Idx = originNode.OsmID, TargetNode = originNode, CumulatedCost = 0};
+                dijkstraSteps.Add(i, firstStep);
+                // dijkstraStepsOrdered.Add(j, firstStep);
+                logger.Debug("Step Key = {0},\tstep Idx = {1},\tprevious node OsmId = {2},\ttarget node OsmId = {3},\tcumulated cost = {4}",
+                    i, dijkstraSteps[i].Idx, dijkstraSteps[i].PreviousStep?.TargetNode?.OsmID, dijkstraSteps[i].TargetNode?.OsmID, dijkstraSteps[i].CumulatedCost);
+                
+                // foreach(var node in reducedSetOfNodes)
+                // {
+                while(!visitedNodes.ContainsKey(destinationNode.Idx))
+                {
+                    var currentStep = dijkstraSteps[j];
+                    var targetNode = currentStep.TargetNode;
+                    if(targetNode != null)
+                    {
+                        foreach(var outwardEdge in targetNode.OutwardEdges)
+                        {
+                            if(!visitedNodes.ContainsKey(outwardEdge.TargetNode.Idx))
+                            {
+                                var dijkstraStep = new DijkstraStep{Idx = outwardEdge.TargetNode.OsmID, PreviousStep = currentStep, TargetNode = outwardEdge.TargetNode, CumulatedCost = outwardEdge.Cost + currentStep.CumulatedCost};
+                                i++;
+                                dijkstraSteps.Add(i, dijkstraStep);
+                                logger.Debug("Step Key = {0},\tstep Idx = {1},\tprevious node OsmId = {2},\ttarget node OsmId = {3},\tcumulated cost = {4}",
+                                    i, dijkstraSteps[i].Idx, dijkstraSteps[i].PreviousStep?.TargetNode?.OsmID, dijkstraSteps[i].TargetNode?.OsmID, dijkstraSteps[i].CumulatedCost);
+                            }
+                        }
+                        visitedNodes.Add(targetNode.Idx, targetNode);
+                    }
+                    j++;
+                }
+                logger.Debug("Destination node reached");
+            }
+
+            logger.Debug("Visited nodes:");
+            foreach(var visitedNode in visitedNodes)
+            {
+                logger.Debug("Visited node OsmId = {0}", visitedNode.Value.OsmID);
+            }
+
+            logger.Debug("Dijkstra steps:");
+            foreach(var step in dijkstraSteps)
+            {
+                logger.Debug("Step Key = {0},\tstep Idx = {1},\tprevious node OsmId = {2},\ttarget node OsmId = {3},\tcumulated cost = {4}",
+                    step.Key, step.Value.Idx, step.Value.PreviousStep?.TargetNode?.OsmID, step.Value.TargetNode?.OsmID, step.Value.CumulatedCost);
+            }
+            
+
+
+            return route;
+        }
+
         private void CreateReducedDataSet(long originNodeOsmId, int maxNumberOfNodes)
         {
             int i = 0;
