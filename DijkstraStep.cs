ac0751d6b5c015b5ee1a0bb2b3420a9fa0bb26b6
Rene Jimenez <rji@synapsis-group.com>
2022-06-22 15:06:47 +0200

    Simplification of DijkstraStep

--- DijkstraStep.cs
+++ DijkstraStep.cs
@@ -3,7 +3,6 @@ namespace SytyRouting
 
     public class DijkstraStep
     {
-        public long Idx { get; set; }
         public DijkstraStep? PreviousStep { get; set; }
         public Node? TargetNode { get; set; }
         public double CumulatedCost { get; set; }
