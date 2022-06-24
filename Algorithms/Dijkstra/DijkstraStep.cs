c53e2fa26e3815f056946110a7557d72f40c7bcd
Rene Jimenez <rji@synapsis-group.com>
2022-06-24 17:01:27 +0200

    Renaming TargetNode to ActiveNode in DijkstraStep

--- Algorithms/Dijkstra/DijkstraStep.cs
+++ Algorithms/Dijkstra/DijkstraStep.cs
@@ -4,7 +4,7 @@ namespace SytyRouting.Algorithms.Dijkstra
     public class DijkstraStep
     {
         public DijkstraStep? PreviousStep { get; set; }
-        public Node? TargetNode { get; set; }
+        public Node? ActiveNode { get; set; }
         public double CumulatedCost { get; set; }
     }
 }
