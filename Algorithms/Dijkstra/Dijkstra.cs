4b0b2d9150a865721e01053e88d09ba6fc3db9ab
Rene Jimenez <rji@synapsis-group.com>
2022-06-30 11:04:08 +0200

    Dijkstra's algorithm with Base class and Interface

--- Algorithms/Dijkstra/Dijkstra.cs
+++ Algorithms/Dijkstra/Dijkstra.cs
@@ -3,68 +3,30 @@ using SytyRouting.Model;
 
 namespace SytyRouting.Algorithms.Dijkstra
 {
-    public class Dijkstra
+    public class Dijkstra : BaseRoutingAlgorithm
     {
         private static Logger logger = LogManager.GetCurrentClassLogger();
-
+        
         private Graph _graph;
         private List<Node> route = new List<Node>();
         private PriorityQueue<DijkstraStep, double> dijkstraStepsQueue = new PriorityQueue<DijkstraStep, double>();
         private Dictionary<int, double> bestScoreForNode = new Dictionary<int, double>();
-
-        public Dijkstra(Graph graph)
+   
+        public Dijkstra(Graph graph) : base(graph)
         {
             _graph = graph;
         }
 
-        public List<Node> GetRoute(double x1, double y1, double x2, double y2)
-        {           
-            var originNode = _graph.GetNodeByLatitudeLongitude(x1, y1);
-            var destinationNode = _graph.GetNodeByLatitudeLongitude(x2, y2);
-            
-            return GetRoute(originNode, destinationNode);
-        }
-
         public void TraceRoute()
         {
+            logger.Debug("Display route Nodes:");
             foreach(Node node in route)
             {
-                logger.Trace("Node OsmId = {0}", node.OsmID);
+                logger.Debug("Node OsmId = {0}", node.OsmID);
             }
         }
 
-        public List<Node> GetRoute(long originNodeOsmId, long destinationNodeOsmId)
-        {
-            Node originNode;
-            try
-            {
-                originNode = _graph.GetNodeByOsmId(originNodeOsmId);
-            }
-            catch (ArgumentException e)
-            {
-                logger.Debug("Origin node (source_osm = {0}) not found", originNodeOsmId);
-                logger.Debug("{0}: {1}", e.GetType().Name, e.Message);
-
-                throw new Exception("Unknown value for node osm id.");
-            }
-
-            Node destinationNode;
-            try
-            {
-                destinationNode = _graph.GetNodeByOsmId(destinationNodeOsmId);
-            }
-            catch (ArgumentException e)
-            {
-                logger.Debug("Destination node (source_osm = {0}) not found", destinationNodeOsmId);
-                logger.Debug("{0}: {1}", e.GetType().Name, e.Message);
-
-                throw new Exception("Unknown value for node osm id.");
-            }
-
-            return GetRoute(originNode, destinationNode);
-        }
-
-        private List<Node> GetRoute(Node originNode, Node destinationNode)
+        protected override List<Node> RouteSearch(Node originNode, Node destinationNode)
         {
             route.Clear();
 
