fc647fe7a94a53ca382d94c8309edc72454644e6
rjisyn <110456764+rjisyn@users.noreply.github.com>
2022-09-16 16:27:21 +0200

    Local {fake} DB for testing

--- Constants.cs
+++ Constants.cs
@@ -18,5 +18,6 @@ namespace SytyRouting
     public static class Constants
     {
         public const string ConnectionString = "Host=compute.syty.io;Port=1234;Username=postgres;Password=test123;Database=sytyrun";
+        public const string LocalConnectionString = "Host=localhost;Port=5432;Username=postgres;Password=;Database=sytyrun_local";
     }
 }
