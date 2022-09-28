906ea7c932a75aca7dfad150b078564f288ca233
rjisyn <110456764+rjisyn@users.noreply.github.com>
2022-09-28 09:19:52 +0200

    Preliminary version of a TransportMode bit mask

--- Constants.cs
+++ Constants.cs
@@ -1,5 +1,56 @@
 namespace SytyRouting
 {
+    [Flags]
+    public enum TransportMode
+    {
+        None = 0,
+        Foot = 1,
+        Bicycle = 2,
+        Car = 4,
+        Bus = 8,
+        Tram = 16,
+        Train = 32,
+    }
+
+    public enum TagId
+    {
+        CyclewayLane = 201,
+        CyclewayOpposite = 204,
+        CyclewayOppositeLane = 203,
+        CyclewayTrack = 202,
+        HighwayBridleway = 120,
+        HighwayBusGuideway = 116,
+        HighwayByway = 121,
+        HighwayCycleway = 118,
+        HighwayFootway = 119,
+        HighwayLivingStreet = 111,
+        HighwayMotorway = 101,
+        HighwayMotorwayJunction = 103,
+        HighwayMotorwayLink = 102,
+        HighwayPath = 117,
+        HighwayPedestrian = 114,
+        HighwayPrimary = 106,
+        HighwayPrimaryLink = 107,
+        HighwayResidential = 110,
+        HighwayRoad = 100,
+        HighwaySecondary = 108,
+        HighwaySecondaryLink = 124,
+        HighwayService = 112,
+        HighwayServices = 115,
+        HighwaySteps = 122,
+        HighwayTertiary = 109,
+        HighwayTertiaryLink = 125,
+        HighwayTrack = 113,
+        HighwayTrunk = 104,
+        HighwayTrunkLink = 105,
+        HighwayUnclassified = 123,
+        JunctionRoundabout = 401,
+        TrackTypeGrade1 = 301,
+        TrackTypeGrade2 = 302,
+        TrackTypeGrade3 = 303,
+        TrackTypeGrade4 = 304,
+        TrackTypeGrade5 = 305
+    }
 
     public enum StepDirection
     {
