ff8eb66ab494456eb6a025e16e114f9dad6f2080
rjisyn <110456764+rjisyn@users.noreply.github.com>
2023-01-16 16:42:10 +0100

    10 000 personas test results

--- Program.cs
+++ Program.cs
@@ -72,6 +72,15 @@ namespace SytyRouting
             var personaRouter = new PersonaRouter(graph);
 
             await personaRouter.StartRouting<SytyRouting.Algorithms.Dijkstra.Dijkstra>();
+
+
+    // 10 000 personas test:
+    //    SytyRouting.PersonaRouter.DBRouteBenchmarkUploadAsync#561   | DEBUG | Transport sequence validation errors: 208 (2.077506991610068 % of the requested transport sequences were overridden)
+    //    SytyRouting.PersonaRouter.DBRouteBenchmarkUploadAsync#562   |  INFO | 9942 Routes successfully uploaded to the database in 0.00:14:50.750 (d.hh:mm:s.ms)
+    //    SytyRouting.PersonaRouter.DBRouteBenchmarkUploadAsync#563   | DEBUG | 70 routes (out of 10012) failed to upload (0.6991610067918498 %)
+    //    SytyRouting.PersonaRouter.DBRouteBenchmarkUploadAsync#564   | DEBUG | 'Origin = Destination' errors: 52 (0.519376747902517 %)
+    //    SytyRouting.PersonaRouter.DBRouteBenchmarkUploadAsync#565   | DEBUG |                  Other errors: 18 (0.1797842588893328 %)
+    //                   SytyRouting.PersonaRouter.StartRouting#92    |  INFO | StartRouting execution time :: 0.00:18:28.007
     
             //personaRouter.TracePersonas();
             // // personaRouter.TracePersonasRouteResult();
