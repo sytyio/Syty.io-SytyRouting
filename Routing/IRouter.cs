bca6f29737a4ae31dbec970575ee234f526cffd9
rjisyn <110456764+rjisyn@users.noreply.github.com>
2023-02-17 17:03:51 +0100

    Code cleaning

--- Routing/IRouter.cs
+++ Routing/IRouter.cs
@@ -5,16 +5,6 @@ namespace SytyRouting.Routing
 {
     public interface IRouter
     {
-        // void Initialize(Graph graph);
-        // List<Node> GetRoute(double originLatitude, double originLongitude, double destinationLatitude, double destinationLongitud, byte[] transportModesSequence);
-        // List<Node> GetRoute(long originNodeOsmId, long destinationNodeOsmId, byte[] transportModesSequence);
-        // List<Node> GetRoute(Node origin, Node destination, byte[] transportModesSequence);
-        // //Dictionary<int,Tuple<byte,int>> GetTransportModeTransitions();
-        // Tuple<string[],DateTime[]> GetTransportModeTransitions();
-        // LineString NodeRouteToLineStringMSeconds(double startX, double startY, double endX, double endY, List<Node> route, TimeSpan initialTimeStamp, DateTime startTime);
-        // LineString TwoPointLineString(double x1, double y1, double x2, double y2, byte transportMode, TimeSpan initialTimeStamp);
-        // Dictionary<int, Tuple<byte,int>> SingleTransportModeTransition(Node origin, Node destination, byte transportMode);
-        // double GetRouteCost();
         byte[] ValidateTransportSequence(int id, Point homeLocation, Point workLocation, string[] transportSequence);
     }
 }
