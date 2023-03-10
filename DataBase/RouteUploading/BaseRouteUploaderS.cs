c9b71c677f0daee4d813461507fec68dbc1d9e63
rjisyn <rji@synapsis-group.com>
2023-03-10 14:04:43 +0100

    Base class for the original static uploader

---
+++ DataBase/RouteUploading/BaseRouteUploaderS.cs
@@ -0,0 +1,17 @@
+using SytyRouting.Model;
+
+namespace SytyRouting.DataBase
+{
+    public abstract class BaseRouteUploaderS : IRouteUploader
+    {
+        public virtual Task<int> UploadRoutesAsync(string connectionString, string auxiliaryTable, string routeTable, List<Persona> personas)
+        {
+           throw new NotImplementedException();
+        }
+
+        public virtual Task<int> UploadRouteAsync(string connectionString, string auxiliaryTable, string routeTable, Persona personas)
+        {
+           throw new NotImplementedException();
+        }
+    }
+}
