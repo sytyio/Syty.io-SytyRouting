f4e072f8f26f782aaa283eb62d1e8a19988b4d88
rjisyn <110456764+rjisyn@users.noreply.github.com>
2023-03-20 14:58:24 +0100

    Transport sequence computation statements explicitly included in the UPDATE batch

--- DataBase/RouteUploading/RouteUploader.cs
+++ DataBase/RouteUploading/RouteUploader.cs
@@ -22,7 +22,6 @@ namespace SytyRouting.DataBase
 
             var auxiliaryTable=routeTable+Configuration.AuxiliaryTableSuffix;
             var auxiliaryTablePK=auxiliaryTable+Configuration.PKConstraintSuffix;
-            var auxiliaryTableFK=auxiliaryTable+Configuration.FKConstraintSuffix;
 
             // Create a factory using default values (e.g. floating precision)
 			GeometryFactory geometryFactory = new GeometryFactory();
@@ -42,7 +41,6 @@ namespace SytyRouting.DataBase
                     new("ALTER TABLE " + auxiliaryTable + " ADD COLUMN IF NOT EXISTS transport_sequence TTEXT(Sequence);")
                 }
             };
-
             await using (var reader = await auxTableBatch.ExecuteReaderAsync())
             {
                 logger.Debug("{0} table creation",auxiliaryTable);
@@ -60,27 +58,6 @@ namespace SytyRouting.DataBase
             await importer.CompleteAsync();
             await importer.CloseAsync();
             
-            //PLGSQL: Iterates over each transport mode transition to create the corresponding temporal text type sequence (ttext(Sequence)) for each valid route
-            // var iterationString = @"
-            // DO 
-            // $$
-            // DECLARE
-            // _id int;
-            // _arr_tm text[];
-            // _arr_ts timestamptz[];
-            // BEGIN    
-            //     FOR _id, _arr_tm, _arr_ts in SELECT persona_id, transport_modes, time_stamps FROM " + auxiliaryTable + @"
-            //     LOOP
-            //         UPDATE " + auxiliaryTable + @" SET transport_sequence= coalesce_transport_modes_time_stamps(_arr_tm, _arr_ts) WHERE is_valid_route = true AND persona_id = _id;
-            //     END LOOP;
-            // END;
-            // $$;
-            // ";
-
-            var iterationString = "UPDATE " + auxiliaryTable + " SET transport_sequence= coalesce_transport_modes_time_stamps(transport_modes, time_stamps) WHERE is_valid_route = true;";
-
-            var updateString = "UPDATE " + routeTable + " r_t SET transport_sequence = aux_t.transport_sequence FROM " + auxiliaryTable + " aux_t WHERE  aux_t.persona_id = r_t.id;";
-
             await using var resultsBatch = new NpgsqlBatch(connection)
             {
                 BatchCommands =
@@ -89,11 +66,10 @@ namespace SytyRouting.DataBase
                     new("UPDATE " + auxiliaryTable + " SET is_valid_route = st_IsValidTrajectory(computed_route);"),
                     new("UPDATE " + auxiliaryTable + " SET is_valid_route = false WHERE st_IsEmpty(computed_route);"),
                     new("UPDATE " + routeTable + " r_t SET route = aux_t.computed_route::tgeompoint FROM " + auxiliaryTable + " aux_t WHERE  aux_t.persona_id = r_t.id AND aux_t.is_valid_route = true;"),
-                    new(iterationString),
-                    new(updateString)
+                    new("UPDATE " + auxiliaryTable + " SET transport_sequence= coalesce_transport_modes_time_stamps(transport_modes, time_stamps) WHERE is_valid_route = true;"),
+                    new("UPDATE " + routeTable + " r_t SET transport_sequence = aux_t.transport_sequence FROM " + auxiliaryTable + " aux_t WHERE  aux_t.persona_id = r_t.id;")
                 }
             };
-
             await using (var reader = await resultsBatch.ExecuteReaderAsync())
             {
                 logger.Debug("{0} table SET statements executed",auxiliaryTable);
